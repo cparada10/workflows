77954cd102b1f0aca8f2e61b37367eefdd931b36
Carlos Parada <cparada@paradavault.com>
2016-11-16 15:48:22 -0800

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'), 
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 // gulp.task('log', function() {
 // 	gutil.log('Workflows are awesome');
 // });
 
 var coffeeSources = ['components/coffee/tagline.coffee']
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+]; // will be processed in array order
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -14,4 +21,10 @@ gulp.task('coffee', function() {
 			.on('error', gutil.log)
 		)
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task( 'js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
