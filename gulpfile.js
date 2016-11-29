1f1ca048776cdbb02eb90b765f89539057ae0884
Carlos Parada <cparada@paradavault.com>
2016-11-28 18:59:19 -0800

    Added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,34 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
 
 // CORE TASKS ----------------------------------------------------------------|
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+// Default tasks
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
 
+// Files to watch
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
+// Live Reload Server
+gulp.task('connect', function() {
+	connect.server({
+		root: 'builds/development',
+		livereload: true
+	});
+});
+
 
 // PROCESS JS ----------------------------------------------------------------|
 
+// Set Coffee and JS files
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
 	'components/scripts/rclick.js',
@@ -28,27 +40,30 @@ var jsSources = [
 	'components/scripts/template.js'
 ]; // will be processed in array order
 
-
+// Coffee Task
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
-		.pipe(coffee({bare: true})
-			.on('error', gutil.log)
-		)
-		.pipe(gulp.dest('components/scripts'))
+		.pipe( coffee({bare: true})
+			.on('error', gutil.log) )
+		.pipe( gulp.dest('components/scripts') )
 });
 
+// JS Task
 gulp.task( 'js', function() {
 	gulp.src(jsSources)
-		.pipe(concat('script.js'))
-		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe( concat('script.js') )
+		.pipe( browserify() )
+		.pipe( gulp.dest('builds/development/js') )
+		.pipe( connect.reload() )
 });
 
 
 // PROCESS SASS --------------------------------------------------------------|
 
+// Set SASS Sources
 var sassSources = ['components/sass/style.scss'];
 
+// SASS Task (+ Compass)
 gulp.task( 'compass', function() {
 	gulp.src(sassSources)
 		.pipe(compass({
@@ -58,5 +73,6 @@ gulp.task( 'compass', function() {
 		})
 		.on('error', gutil.log))
 		.pipe(gulp.dest('builds/development/css'))
+		.pipe( connect.reload() )
 });
 
